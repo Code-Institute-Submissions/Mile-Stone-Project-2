d483c8b074ac4ba5e8237bfeb7f73334f8a282ee
Thomas Dodson <tw.dodson@yahoo.co.uk>
2018-11-04 00:21:08 +0000

    more work on graphs

--- assets/js/graphs-scripts-1.js
+++ assets/js/graphs-scripts-1.js
@@ -2,24 +2,51 @@ queue()
     .defer(d3.csv, "data/projectTwoBeerNumbers.csv")
     .await(makeGraphs);
 
-function makeGraphs(error, projectTwoBeerNumbersData) {
+
+function makeGraphs(error, countryData) {
+    var ndx = crossfilter(countryData);
+
+    countryData.forEach(function(d) {
+        d.year = parseInt(d.year);
+        d.country = parseInt(d["country"]);
+        d.active_breweries = parseInt(d["active.breweries"]);
+        d.volume = parseInt(d.volume);
+    })
+
+    show_year_selector(ndx);
+
+    show_volume_by_country(ndx);
+    //sales_growth_by_country(ndx);
+    //number_of_active_breweries(ndx);
     
-    var ndx = crossfilter(projectTwoBeerNumbersData);
-    var country_dim = ndx.dimension(dc.pluck('country'));
-    var volume_by_country = country_dim.group().reduceSum(dc.pluck('volume'));
-    console.log(ndx);
+    dc.renderAll();
+}
+
+function show_year_selector(ndx) {
+    var dim = ndx.dimension(dc.pluck('year'));
+    var group = dim.group();
+
+    dc.selectMenu("#year-selector")
+        .dimension(dim)
+        .group(group);
+}
+
+
+function show_volume_by_country(ndx) {
+    var dim = ndx.dimension(dc.pluck('volume'));
+    var group = dim.group();
+
     dc.barChart("#volume-country")
-            .width(600)
-            .height(300)
-            .margins({top: 10, right: 50, bottom: 30, left: 50})
-            .dimension(country_dim)
-            .group(volume_by_country)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("country")
-            .yAxisLabel("volume")
-            .yAxis().ticks(12);
-        dc.renderAll();
+        .width(350)
+        .height(250)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Country")
+        .yAxisLabel("volume")
+        .yAxis().ticks(20);
 }
 
