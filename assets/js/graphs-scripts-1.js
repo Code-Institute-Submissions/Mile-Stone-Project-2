4490c62eeaf402473910932834f91e909e7ae788
Thomas Dodson <tw.dodson@yahoo.co.uk>
2018-11-09 17:25:21 +0000

    latest version for tutorial

--- assets/js/graphs-scripts-1.js
+++ assets/js/graphs-scripts-1.js
@@ -10,36 +10,61 @@ function makeGraphs(error, countryData) {
         d.country = parseInt(d.country);
         d.active_breweries = parseInt(d.active_breweries);
         d.volume = parseInt(d.volume);
+       
     })
 
-    //var country_dim = ndx.dimension(dc.pluck('country'));
-    var year_group = ndx.dimension(dc.pluck('year'));
-    
-    var groupingData = year_group.dim().reduceSum(function(d){
-        if (d.country === 'year') {
-            return +d.country;
-        } else {
-            return 0;
-        }
-    });
-
     show_year_selector(ndx);
+    volume_by_country(ndx);
+    active_breweries_per_country(ndx);
     
     dc.renderAll();
 }
 
 function show_year_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('year'));
-    var group = dim.group();
+    var group = dim.group(dc.pluck('volume'));
 
     dc.selectMenu("#year-selector")
         .dimension(dim)
         .group(group);
 }
 
+function volume_by_country(ndx) {
+    var dim = ndx.dimension(dc.pluck('country')); //at bottom
+    var group = dim.group(dc.pluck('volume'));
+
+    dc.barChart("#volume-country")
+        .width(350)
+        .height(250)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Country")
+        .yAxisLabel("Volume")
+        .yAxis().ticks(20);
+}      
 
 
+function active_breweries_per_country(ndx) {
+    var dim = ndx.dimension(dc.pluck('country')); //at bottom
+    var group = dim.group(dc.pluck('active-breweries'));
 
+    dc.barChart("#active-breweries-eu")
+        .width(350)
+        .height(250)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Country")
+        .yAxisLabel("Active Breweries")
+        .yAxis().ticks(20);
+}
 
 
 
