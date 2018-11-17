ff66e3e4903136b44322d81e5c4bf66ca14b2c2c
Thomas Dodson <tw.dodson@yahoo.co.uk>
2018-11-17 23:02:16 +0000

    added pie chart

--- assets/js/graphs-scripts-1.js
+++ assets/js/graphs-scripts-1.js
@@ -1,26 +1,28 @@
 queue()
     .defer(d3.csv, "data/project-two-data.csv")
     .await(makeGraphs);
-    
+
 function makeGraphs(error, beerData) {
     var ndx = crossfilter(beerData);
 
-    beerData.forEach(function(d) {
-        d.brewery = parseInt(d.brewery);
-        d.beer_name = parseInt(d.beer_name);
-        d.brewery_location = parseInt(d.brewery_location);
-        d.pints_bottles_sold = parseInt(d.pints_bottles_sold)
-        d.beer_type = parseInt(d.beer_type)
-    })
+    //beerData.forEach(function(d) {
+        
+        //d.beer_name = parseInt(d.beer_name);
+        //d.brewery_location = parseInt(d.brewery_location);
+        //d.pints_bottles_sold = parseInt(d.pints_bottles_sold)
+        //d.beer_type = parseInt(d.beer_type)
+    //})
 
     show_county_selector(ndx);
     show_category_selector(ndx);
     show_brewery_selector(ndx);
     show_volume_by_county(ndx);
+    show_volume_by_brewery(ndx);
     show_volume_by_category(ndx);
+    pie_chart_category(ndx);
 
     dc.renderAll();
-}    
+}
 
 function show_county_selector(ndx) {
     var dim = ndx.dimension(dc.pluck('brewery-location'));
@@ -42,7 +44,7 @@ function show_category_selector(ndx) {
 
 //does this need a custom reduce?
 function show_brewery_selector(ndx) {
-    var dim = ndx.dimension(dc.pluck('brewery'));
+    var dim = ndx.dimension(dc.pluck('brewery-name'));
     var group = dim.group();
 
     dc.selectMenu("#brewery-selector")
@@ -65,9 +67,28 @@ function show_volume_by_category(ndx) {
         .xUnits(dc.units.ordinal)
         .xAxisLabel("Location")
         .yAxisLabel("Volume")
+        .xAxis(d3.svg.axis())
         .yAxis().ticks(10);
 }
 //var total_spend_per_person = name_dim.group().reduceSum(dc.pluck('spend'));
+
+function show_volume_by_brewery(ndx) {
+    var dim = ndx.dimension(dc.pluck('brewery-name')); //at bottom
+    var group = dim.group().reduceSum(dc.pluck('pints-bottles-sold'));
+
+    dc.barChart("#volume-by-brewery")
+        .width(500)
+        .height(250)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(dim)
+        .group(group)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Brewery")
+        .yAxisLabel("Volume")
+        .yAxis().ticks(10);
+}
 
 function show_volume_by_county(ndx) {
     var dim = ndx.dimension(dc.pluck('beer-type')); //at bottom
@@ -85,4 +106,16 @@ function show_volume_by_county(ndx) {
         .xAxisLabel("Category")
         .yAxisLabel("Volume")
         .yAxis().ticks(10);
+}
+
+function pie_chart_category(ndx) {
+    var dim = ndx.dimension(dc.pluck('beer-type'));
+    var group = dim.group().reduceSum(dc.pluck('pints-bottles-sold'));
+    dc.pieChart('#category-percentage')
+        .height(330)
+        .radius(90)
+        .legend(dc.legend().x(250).y(120).itemHeight(10).gap(5))
+        .transitionDuration(1500)
+        .dimension(dim)
+        .group(group);
 }
